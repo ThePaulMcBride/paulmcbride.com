4181707ea1ad9ccea6d9d1259a11d395218790a6
Paul McBride <hello@paulmcbride.net>
2022-05-09 18:32:17 +0000

    add tool for generating rss feed

---
+++ lib/feed.ts
@@ -0,0 +1,44 @@
+import { Feed } from "feed";
+import { allPosts } from "contentlayer/generated";
+import { parseISO } from "date-fns";
+
+const author = {
+  name: "Paul McBride",
+  email: "hello@paulmcbride.net",
+  link: "https://paulmcbride.com",
+};
+
+export const buildFeed = async () => {
+  const feed = new Feed({
+    title: "PaulMcBride.com",
+    description: "The tech ramblings of Paul McBride",
+    id: "https://paulmcbride.com/",
+    link: "https://paulmcbride.com/",
+    language: "en-GB",
+    image: "https://paulmcbride.com/static/banner.jpeg",
+    favicon: "https://paulmcbride.com/static/favicons/favicon.ico",
+    copyright: `All rights reserved ${new Date().getFullYear()}, Paul McBride`,
+    feedLinks: {
+      json: "https://paulmcbride.com/json",
+      atom: "https://paulmcbride.com/atom",
+    },
+    author,
+  });
+
+  allPosts.forEach((post) => {
+    feed.addItem({
+      title: post.title,
+      id: post.slug,
+      link: `https://paulmcbride.com/posts/${post.slug}`,
+      description: post.description,
+      content: `<p>${post.teaser}</p><div style="margin-top: 50px; font-style: italic;"> <strong><a href="https://paulmcbride.com/posts/${post.slug}">Keep reading</a>.</strong> </div> <br /> <br />`,
+      author: [author],
+      date: parseISO(post.date),
+      image: `https://paulmcbride.com${post.banner}`,
+    });
+  });
+
+  feed.addContributor(author);
+
+  return feed;
+};
