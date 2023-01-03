42f4911d6ca7b9009d25fa8a84f486fd08a58809
Paul McBride <hello@paulmcbride.net>
2023-01-03 11:17:09 +0000

    remove width limit from top nav

--- components/Container.tsx
+++ components/Container.tsx
@@ -89,7 +89,7 @@ export default function Container(props: any) {
         )}
       </Head>
       <div className="flex flex-col justify-center px-8">
-        <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
+        <nav className="flex items-center justify-between w-full relative border-gray-200 dark:border-gray-700 mx-auto pt-7 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
           <a href="#skip" className="skip-nav">
             Skip to content
           </a>
