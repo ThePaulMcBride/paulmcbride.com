c5713c5dd4d42ef0b1893bcf777b713d49f709d0
Paul McBride <hello@paulmcbride.com>
2024-04-20 18:11:17 +0100

    feat: get posts listed on homepage

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,16 @@
-// 1. Import utilities from `astro:content`
 import { z, defineCollection } from "astro:content";
 
-// 2. Define a `type` and `schema` for each collection
 const posts = defineCollection({
-  type: "content", // v2.5.0 and later
+  type: "content",
   schema: z.object({
     title: z.string(),
     date: z.string(),
     description: z.string(),
     banner: z.string(),
-    tags: z.array(z.string()),
+    tags: z.array(z.string()).optional(),
   }),
 });
 
-// 3. Export a single `collections` object to register your collection(s)
 export const collections = {
   posts: posts,
 };
