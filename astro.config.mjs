2ed4692d89ebc0d2fd7d727ecd7aa0fe12f03b3f
Paul McBride <hello@paulmcbride.com>
2024-04-20 18:11:56 +0100

    feat: get page layout rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [mdx()],
+  output: "server",
+  integrations: [mdx(), tailwind()],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -3,17 +3,17 @@ import { z, defineCollection } from "astro:content";
 
 // 2. Define a `type` and `schema` for each collection
 const posts = defineCollection({
-	type: "content", // v2.5.0 and later
-	schema: z.object({
-		title: z.string(),
-		date: z.string(),
-		description: z.string(),
-		banner: z.string(),
-		tags: z.array(z.string()),
-	}),
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+    description: z.string(),
+    banner: z.string(),
+    tags: z.array(z.string()),
+  }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-	posts: posts,
+  posts: posts,
 };
