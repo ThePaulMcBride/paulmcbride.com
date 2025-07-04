1a2b1b8801378090e9f37a7058167127c4d127c1
Paul McBride <hello@paulmcbride.com>
2025-07-03 18:32:17 -0700

    wip: scaffold actix app

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
